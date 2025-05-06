015f41fb71b2a29b14e171970903eb987e1aec4a
Alex3584 <kolesnyk05071984@gmail.com>
2025-05-06 12:21:56 -0300

    feat: integrate HTML5UP template into Laravel using Blade layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Editorial')</title>
+    @vite(['resources/sass/main.scss'])
+    @stack('styles')
+</head>
+
+<body class="is-preload">
+    @yield('content')
+
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/browser.min.js') }}"></script>
+    <script src="{{ asset('assets/js/breakpoints.min.js') }}"></script>
+    <script src="{{ asset('assets/js/util.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+    @stack('scripts')
+</body>
+
+</html>
